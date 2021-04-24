8376f30c26ef48fcda4a7dcfd980d3c0c1d9a0b7
Pratyush Mishra <pratyushmishra@berkeley.edu>
2021-04-24 11:18:46 -0700

    Make `PedersenCompressor` a `TwoToOneCRH`

--- src/crh/injective_map/mod.rs
+++ src/crh/injective_map/mod.rs
@@ -3,7 +3,7 @@ use ark_ff::bytes::ToBytes;
 use ark_std::rand::Rng;
 use ark_std::{fmt::Debug, hash::Hash, marker::PhantomData};
 
-use super::{pedersen, CRH};
+use super::{pedersen, CRH, TwoToOneCRH};
 use ark_ec::{
     models::{ModelParameters, TEModelParameters},
     twisted_edwards_extended::{GroupAffine as TEAffine, GroupProjective as TEProjective},
@@ -45,14 +45,41 @@ impl<C: ProjectiveCurve, I: InjectiveMap<C>, W: pedersen::Window> CRH
 
     fn setup<R: Rng>(rng: &mut R) -> Result<Self::Parameters, Error> {
         let time = start_timer!(|| format!("PedersenCRHCompressor::Setup"));
-        let params = pedersen::CRH::<C, W>::setup(rng);
+        let params = <pedersen::CRH<C, W> as CRH>::setup(rng);
         end_timer!(time);
         params
     }
 
     fn evaluate(parameters: &Self::Parameters, input: &[u8]) -> Result<Self::Output, Error> {
         let eval_time = start_timer!(|| "PedersenCRHCompressor::Eval");
-        let result = I::injective_map(&pedersen::CRH::<C, W>::evaluate(parameters, input)?)?;
+        let result = I::injective_map(&<pedersen::CRH<C, W> as CRH>::evaluate(parameters, input)?)?;
+        end_timer!(eval_time);
+        Ok(result)
+    }
+}
+
+impl<C: ProjectiveCurve, I: InjectiveMap<C>, W: pedersen::Window> TwoToOneCRH
+    for PedersenCRHCompressor<C, I, W>
+{
+    const LEFT_INPUT_SIZE_BITS: usize = pedersen::CRH::<C, W>::LEFT_INPUT_SIZE_BITS;
+    const RIGHT_INPUT_SIZE_BITS: usize = pedersen::CRH::<C, W>::RIGHT_INPUT_SIZE_BITS;
+    type Output = I::Output;
+    type Parameters = pedersen::Parameters<C>;
+
+    fn setup<R: Rng>(r: &mut R) -> Result<Self::Parameters, Error> {
+        <pedersen::CRH<C, W> as TwoToOneCRH>::setup(r)
+    }
+
+    /// A simple implementation method: just concat the left input and right input together
+    ///
+    /// `evaluate` requires that `left_input` and `right_input` are of equal length.
+    fn evaluate(
+        parameters: &Self::Parameters,
+        left_input: &[u8],
+        right_input: &[u8],
+    ) -> Result<Self::Output, Error> {
+        let eval_time = start_timer!(|| "PedersenCRHCompressor::Eval");
+        let result = I::injective_map(&<pedersen::CRH<C, W> as TwoToOneCRH>::evaluate(parameters, left_input, right_input)?)?;
         end_timer!(eval_time);
         Ok(result)
     }
