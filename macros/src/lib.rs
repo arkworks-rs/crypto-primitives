4932afffa2e4c0df0535b752c6cb31ba51556c45
mmagician <marcin.gorny.94@protonmail.com>
2024-01-25 19:02:45 +0100

    implement `Absorb` macro for structs with unnamed fields too

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,7 +1,8 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Index};
 
 #[proc_macro_derive(Absorb)]
 pub fn derive_absorb(input: TokenStream) -> TokenStream {
@@ -10,43 +11,84 @@ pub fn derive_absorb(input: TokenStream) -> TokenStream {
     let name = input.ident;
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
-    let absorb_impl = match input.data {
-        Data::Struct(data_struct) => {
-            match data_struct.fields {
-                Fields::Named(ref fields) => {
-                    let absorb_fields = fields.named.iter().map(|f| {
-                        let name = &f.ident;
-                        quote! {
-                            Absorb::to_sponge_bytes(&self.#name, dest);
-                        }
-                    });
-
-                    let absorb_fields_elements = fields.named.iter().map(|f| {
-                        let name = &f.ident;
-                        quote! {
-                            Absorb::to_sponge_field_elements(&self.#name, dest);
-                        }
-                    });
-
-                    quote! {
-                        impl #impl_generics Absorb for #name #ty_generics #where_clause {
-                            fn to_sponge_bytes(&self, dest: &mut Vec<u8>) {
-                                #( #absorb_fields )*
-                            }
-
-                            fn to_sponge_field_elements<F: PrimeField>(&self, dest: &mut Vec<F>) {
-                                #( #absorb_fields_elements )*
-                            }
-                        }
-                    }
-                }
-                // Handle other field types (Unnamed, Unit) if necessary...
-                _ => panic!("Absorb only supports named fields"),
-            }
-        }
-        // Handle other data types (Enum, Union) if necessary...
-        _ => panic!("DeriveAbsorb only supports structs"),
+    let len = if let Data::Struct(ref data_struct) = input.data {
+        data_struct.fields.len()
+    } else {
+        panic!(
+            "`Absorb` can only be derived for structs, {} is not a struct",
+            name
+        );
     };
 
-    TokenStream::from(absorb_impl)
+    let mut to_sponge_bytes = Vec::<TokenStream2>::with_capacity(len);
+    let mut to_sponge_field_elements = Vec::<TokenStream2>::with_capacity(len);
+
+    match input.data {
+        Data::Struct(ref data_struct) => match data_struct.fields {
+            Fields::Named(ref fields) => {
+                let _: Vec<_> = fields
+                    .named
+                    .iter()
+                    .map(|f| {
+                        let name = &f.ident;
+                        to_sponge_bytes.push(quote! {
+                            Absorb::to_sponge_bytes(&self.#name, dest);
+                        });
+                    })
+                    .collect();
+
+                let _: Vec<_> = fields
+                    .named
+                    .iter()
+                    .map(|f| {
+                        let name = &f.ident;
+                        to_sponge_field_elements.push(quote! {
+                            Absorb::to_sponge_field_elements(&self.#name, dest);
+                        });
+                    })
+                    .collect();
+            }
+            Fields::Unnamed(ref fields) => {
+                let _: Vec<_> = fields
+                    .unnamed
+                    .iter()
+                    .enumerate()
+                    .map(|(i, _)| {
+                        let index = Index::from(i);
+                        to_sponge_bytes.push(quote! {
+                            Absorb::to_sponge_bytes(&self.#index, dest);
+                        });
+                    })
+                    .collect();
+
+                let _: Vec<_> = fields
+                    .unnamed
+                    .iter()
+                    .enumerate()
+                    .map(|(i, _)| {
+                        let index = Index::from(i);
+                        to_sponge_field_elements.push(quote! {
+                            Absorb::to_sponge_field_elements(&self.#index, dest);
+                        });
+                    })
+                    .collect();
+            }
+            _ => panic!("Absorb only supports named and unnamed fields"),
+        },
+        // this should be unreachable, we already checked
+        _ => panic!("Absorb only supports structs"),
+    }
+
+    let gen = quote! {
+        impl #impl_generics Absorb for #name #ty_generics #where_clause {
+            fn to_sponge_bytes(&self, dest: &mut Vec<u8>) {
+                #( #to_sponge_bytes )*
+            }
+
+            fn to_sponge_field_elements<F: PrimeField>(&self, dest: &mut Vec<F>) {
+                #( #to_sponge_field_elements )*
+            }
+        }
+    };
+    TokenStream::from(gen)
 }
